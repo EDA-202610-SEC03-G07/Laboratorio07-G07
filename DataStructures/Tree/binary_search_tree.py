c7a26f62b4652f159b2866532d98b57a43c0257f
Gabriel Moya <itsgamc@gmail.com>
2026-04-09 10:31:37 -0500

    size

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -3,20 +3,23 @@
 def new_bst():
     return {"root": None}
 
+def insert_node(root, key, value):
+    if root is None:
+        root = new_node(key, value)
+    else :
+        if key < root['key']:
+            root['left'] = insert_node(root['left'], key, value)
+        elif key > root['key']:
+            root['right'] = insert_node(root['right'], key, value)
+        else:
+            root['value'] = value 
+    
 
+def put(bst, key, value):
 
-
-
-
-
-
-
-
-
-
-
-
-
+    bst['root'] = insert_node(bst['root'], key, value )
+    
+    return bst
 
 def size(bts):
     return size_tree(bts["root"])
@@ -25,6 +28,6 @@ def size_tree(my_node):
     if my_node is None:
         return 0
     else:
-        return my_node["size"]
+        return 1+size_tree(my_node["left"])+size_tree(my_node["right"])
     
     
