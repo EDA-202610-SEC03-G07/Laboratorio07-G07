da111721d63d743a5e0d02513bb4e89619d4b72b
G3r07 <g.rochal2@uniandes.edu.co>
2026-04-09 10:43:08 -0500

    funcion get

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,11 +1,11 @@
-
+import bst_node as bst
 
 def new_bst():
     return {"root": None}
 
 def insert_node(root, key, value):
     if root is None:
-        root = new_node(key, value)
+        root = bst.new_node(key, value)
     else :
         if key < root['key']:
             root['left'] = insert_node(root['left'], key, value)
@@ -13,7 +13,8 @@ def insert_node(root, key, value):
             root['right'] = insert_node(root['right'], key, value)
         else:
             root['value'] = value 
-    
+        root["size"]=1+ size_tree(root["left"]) + size_tree(root["right"])
+    return root
 
 def put(bst, key, value):
 
@@ -21,7 +22,7 @@ def put(bst, key, value):
     
     return bst
 
-def size(bts):
+def size(bst):
     return size_tree(bts["root"])
 
 def size_tree(my_node):
@@ -29,5 +30,20 @@ def size_tree(my_node):
         return 0
     else:
         return 1+size_tree(my_node["left"])+size_tree(my_node["right"])
+    
+def get(bst,key):
+    return get_node(bst["root"],key["value"])
+
+def get_node(root,key):
+    if root is None:
+        return root
+    else:
+        if root["key"] ==key:
+            return root
+        elif root["key"]< key:
+            return get_node(root["left"],key)
+        else:
+            return get_node(root["right"],key)
+            
     
     
