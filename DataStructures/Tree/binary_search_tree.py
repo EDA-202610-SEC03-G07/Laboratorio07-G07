82e0c594af15d621e6df055b5f598caebaf7467f
edgonzalez8 <ed.gonzalezm1@uniandes.edu.co>
2026-04-09 10:25:41 -0500

    asas

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,17 +1,30 @@
-def insert_node(root, key, value):
-    if root is None:
-        root = new_node(key, value)
-    else :
-        if key < root['key']:
-            root['left'] = insert_node(root['left'], key, value)
-        elif key > root['key']:
-            root['right'] = insert_node(root['right'], key, value)
-        else:
-            root['value'] = value 
-    
 
-def put(bst, key, value):
 
-    bst['root'] = insert_node(bst['root'], key, value )
+def new_bst():
+    return {"root": None}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def size(bts):
+    return size_tree(bts["root"])
+
+def size_tree(my_node):
+    if my_node is None:
+        return 0
+    else:
+        return my_node["size"]
     
-    return bst
+    
